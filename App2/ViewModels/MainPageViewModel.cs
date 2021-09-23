8713515d6e9a43b58d0d8189f92a98329901cfff
JORGE ARMANDO JUAREZ VAZQUEZ <2018371036@uteq.edu.mx>
2021-09-22 23:52:08 -0500

    Agregar archivos de proyecto.

---
+++ App2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App2.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    { 
+        private String _nombre;
+
+        public MainPageViewModel()
+        {
+            _nombre = "Hola";
+        }
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (string.Equals(_nombre, value)) return;
+
+                _nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(SinUpdate));
+            }
+        }
+
+        public string SinUpdate
+        {
+            get => _nombre;
+        }
+    }
+}
